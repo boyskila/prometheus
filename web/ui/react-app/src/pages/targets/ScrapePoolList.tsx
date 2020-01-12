2ef94108b1748e0e9caacd16883e14df4b21f62c
blalov <boiskila@gmail.com>
2020-01-12 03:29:01 +0200

    refactor targets page to use withStatusIndicator

--- web/ui/react-app/src/pages/targets/ScrapePoolList.tsx
+++ web/ui/react-app/src/pages/targets/ScrapePoolList.tsx
@@ -1,60 +1,49 @@
 import React, { FC } from 'react';
 import { FilterData } from './Filter';
 import { useFetch } from '../../utils/useFetch';
-import { ScrapePool, groupTargets, Target } from './target';
+import { groupTargets, Target } from './target';
 import ScrapePoolPanel from './ScrapePoolPanel';
 import PathPrefixProps from '../../PathPrefixProps';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { Alert } from 'reactstrap';
-
-interface TargetsResponse {
-  activeTargets: Target[];
-  droppedTargets: Target[];
-}
+import { withStatusIndicator } from '../../withStatusIndicator';
 
 interface ScrapePoolListProps {
   filter: FilterData;
+  activeTargets: Target[];
 }
 
-const filterByHealth = ({ upCount, targets }: ScrapePool, { showHealthy, showUnhealthy }: FilterData): boolean => {
-  const isHealthy = upCount === targets.length;
-  return (isHealthy && showHealthy) || (!isHealthy && showUnhealthy);
-};
+const ScrapePoolListWithStatusIndicator = withStatusIndicator<ScrapePoolListProps>(({ filter, activeTargets }) => {
+  const targetGroups = groupTargets(activeTargets);
+  const { showHealthy, showUnhealthy } = filter;
+  return (
+    <>
+      {Object.keys(targetGroups).reduce<JSX.Element[]>((panels, scrapePool) => {
+        const targetGroup = targetGroups[scrapePool];
+        const isHealthy = targetGroup.upCount === targetGroup.targets.length;
+        return (isHealthy && showHealthy) || (!isHealthy && showUnhealthy)
+          ? [...panels, <ScrapePoolPanel key={scrapePool} scrapePool={scrapePool} targetGroup={targetGroup} />]
+          : panels;
+      }, [])}
+    </>
+  );
+});
 
-const ScrapePoolList: FC<ScrapePoolListProps & PathPrefixProps> = ({ filter, pathPrefix }) => {
-  const { response, error } = useFetch<TargetsResponse>(`${pathPrefix}/api/v1/targets?state=active`);
-
-  if (error) {
-    return (
-      <Alert color="danger">
-        <strong>Error fetching targets:</strong> {error.message}
-      </Alert>
-    );
-  } else if (response && response.status !== 'success' && response.status !== 'start fetching') {
-    return (
-      <Alert color="danger">
-        <strong>Error fetching targets:</strong> {response.status}
-      </Alert>
-    );
-  } else if (response && response.data) {
-    const { activeTargets } = response.data;
-    const targetGroups = groupTargets(activeTargets);
-    return (
-      <>
-        {Object.keys(targetGroups)
-          .filter((scrapePool: string) => filterByHealth(targetGroups[scrapePool], filter))
-          .map((scrapePool: string) => {
-            const targetGroupProps = {
-              scrapePool,
-              targetGroup: targetGroups[scrapePool],
-            };
-            return <ScrapePoolPanel key={scrapePool} {...targetGroupProps} />;
-          })}
-      </>
-    );
-  }
-  return <FontAwesomeIcon icon={faSpinner} spin />;
+const ScrapePoolList: FC<{ filter: FilterData } & PathPrefixProps> = ({ pathPrefix, filter }) => {
+  const { response, error, isLoading } = useFetch<Partial<ScrapePoolListProps>>(`${pathPrefix}/api/v1/targets?state=active`);
+  const { status: responseStatus } = response;
+  const badResponseStatus = responseStatus !== 'success' && responseStatus !== 'start fetching';
+  return (
+    <ScrapePoolListWithStatusIndicator
+      {...response.data}
+      filter={filter}
+      error={badResponseStatus ? new Error() : error}
+      isLoading={isLoading}
+      customErrorMsg={
+        <>
+          <strong>Error fetching targets:</strong> {badResponseStatus ? responseStatus : error && error.message}
+        </>
+      }
+    />
+  );
 };
 
 export default ScrapePoolList;
